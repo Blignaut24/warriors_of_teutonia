322a1e749fe7028065e0db86cf39273f3b1a1ae1
Johann Jurgens Blignaut <112351511+Blignaut24@users.noreply.github.com>
2024-02-06 11:17:08 +0100

    feat: Implement and Modify JavaScript Modal
    This commit includes the following changes:
    
    - Created a basic JavaScript modal extracted from [w3schools    guide](https://www.w3schools.com/howto/howto_css_modals.asp) and made necessary modifications.
    - Updated the rules button, originally sourced from [Temani Afif's button89](https://getcssscan.com/css-buttons-examples), to better suit our needs.
    - Successfully passed function tests for the implemented modal.
    
    This commit enhances the user interface by introducing a responsive and interactive modal that displays rules as notifications.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   assets/css/style.css
    	modified:   assets/js/script.js
    	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 // Define game icons
 const hammerIcon = document.getElementById("playerHammer");
 hammerIcon.addEventListener("click", () => select("hammer"));
@@ -173,3 +172,31 @@ function select(playerChoice) {
 
 // Set initial values on startup
 resetAll();
+
+// Rules Modal code snippet modified from the source: https://www.w3schools.com/howto/howto_css_modals.asp
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
